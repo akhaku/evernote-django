97e0b8b517fa5af173572d8c3648aed546fdbbb0
Eliot Alter <Eliot.Alter@tufts.edu>
2012-04-14 17:05:55 -0400

    Added more to vim.js

--- evervim/static/js/vim.js
+++ evervim/static/js/vim.js
@@ -1,16 +1,53 @@
+//Global variables
+var mode = MODE.NORMAL;
+var row = 0;
+var col = 0;
+
 $(document).ready(function() {
-  var textField = $("#vimText");
-  textField.text("hello world");
+  //var textField = $("#vimText");
+  //textField.text("hello world");
   $(document).keypress(function (event) {
       event.preventDefault();
       keyPress(String.fromCharCode(event.which))
   });
-  console.log(textToLineList(textField.text(), 3));
+  $(document).keyup(function (event) {
+      if(event.which == KEYCODE.ESCAPE)
+        vim_escape();
+  });
+  //console.log(textToLineList(textField.text(), 3));
 });
+
+var KEYCODE = {
+  ESCAPE: 33
+};
+
+var MODE = {
+  NORMAL: 0,
+  INSERT: 1
+};
+
 
 function keyPress(c)
 {
+  if(mode == MODE.INSERT) {
+    console.log("In insert mode with key " + c);
+  }
+  else if(mode == MODE.NORMAL) {
+    if(c == 'i') {
+      mode = MODE.INSERT;
+    }
+  }
   console.log("Key " + c);
+}
+
+function vim_escape()
+{
+  mode = MODE.NORMAL;
+}
+
+function insertCharacter(c)
+{
+  //TODO: work here
 }
 
 function textToLineList(text, lineLength)
